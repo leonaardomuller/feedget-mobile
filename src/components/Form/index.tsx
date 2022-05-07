c05332fe365cef77f85c4b0dfa5a215c5d0ccc8d
leonaardomuller <leonaardo.muller@gmail.com>
2022-05-07 00:39:56 -0300

    :tada: first commit

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { ArrowLeft } from "phosphor-react-native";
+import { View, TextInput, Image, Text, TouchableOpacity } from "react-native";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { FeedbackType } from "../Widget";
+import { Button } from "../Button";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { captureScreen } from "react-native-view-shot";
+
+import { theme } from "../../theme";
+import { styles } from "./styles";
+
+interface Props {
+  feedbackType: FeedbackType;
+}
+
+export function Form({ feedbackType }: Props) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleScreenshot() {
+    captureScreen({
+      format: "jpg",
+      quality: 0.8,
+    })
+      .then((uri) => {
+        setScreenshot(uri);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function handleRemoveScreenshot() {
+    setScreenshot(null);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <ArrowLeft
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+        <View style={styles.titleContainer}>
+          <Image source={feedbackTypeInfo.image} style={styles.image} />
+          <Text style={styles.titleText}>{feedbackTypeInfo.title}</Text>
+        </View>
+      </View>
+
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo."
+        placeholderTextColor={theme.colors.text_secondary}
+      />
+
+      <View style={styles.footer}>
+        <ScreenshotButton
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleRemoveScreenshot}
+          screenshot={screenshot}
+        />
+        <Button isLoading={false} />
+      </View>
+    </View>
+  );
+}
